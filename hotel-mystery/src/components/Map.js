f81d73fca082c6225f1205852e88b85d9928895f
Sviktam1 <47092586+Sviktam1@users.noreply.github.com>
2020-01-11 15:36:09 -0500

    Added canvas componenet with a map

--- hotel-mystery/src/components/Map.js
+++ hotel-mystery/src/components/Map.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import Floor1 from './floor1.png';
+
 class Map extends React.Component {
     componentDidMount() {
-        const canvas = this.refs.canvas
-        const ctx = canvas.getContext("2d")
-        const img = this.refs.image;
-        ctx.drawImage(img, 10, 10);
-        img.onload = () => {
-            ctx.drawImage(img, 0, 0)
-            ctx.font = "40px Courier"
-            ctx.fillText("TEST", 210, 75)
-          }
-        }
-    render(){
-        return (
-            <div className="int-map">
-                <canvas ref="canvas" width="450px" height="850px"></canvas>
-                <img ref="image" src="../../public/floor1.png" className="hidden" alt="floor1"/>
-            </div>
-        )
+      const canvas = this.refs.canvas
+      const ctx = canvas.getContext("2d")
+      const img = this.refs.image
+      img.onload = () => {
+        ctx.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+            0, 0, canvas.width, canvas.height)
+        ctx.font = "40px Courier"
+      }
     }
-}
+    render() {
+      return(
+        <div>
+          <canvas ref="canvas" width="1100px" height="700px" />
+          <img ref="image" src={Floor1} className="hidden" />
+        </div>
+      )
+    }
+  }
 
 export default Map;
